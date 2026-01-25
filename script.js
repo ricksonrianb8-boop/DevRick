3d4c430004d2c7280eb8103b26ded55a89b9ede5
rickson rian <ricksonrianb8@gmail.com>
2026-01-24 22:50:03 -0300

    avatres

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function toggleMode() {
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./ASSETS/avatar-light.png")
+    img.setAttribute("src", "./ASSETS/avatar-light.png.png")
   } else {
     // se tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./ASSETS/avatar.png")
